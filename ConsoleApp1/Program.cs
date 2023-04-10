3fd6912a2333589a788b6b312d53983c221b15b0
samatamakiko <makiko.samata@keycrew.co.jp>
2023-04-10 22:39:22 +0900

    CSVHelper使ってコンソール出力 ちゃんとヘッダーもつけることができた

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ using System.Globalization;
 using System.IO;
 using System.Text;
 using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
 
 namespace test
 {
@@ -10,30 +12,32 @@ namespace test
     {
         static void Main(string[] args)
         {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+            };
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-            using (var reader = new StreamReader(@"C:\Users\makik\Downloads\test.csv",Encoding.GetEncoding("Shift-JIS")))
-            { 
-                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            using (var reader = new StreamReader(@"C:\Users\makik\Downloads\test.csv", Encoding.GetEncoding("Shift-JIS"))) 
+            using (var csv = new CsvReader(reader, config))
+            {
+                //ヘッダー取得
+                csv.Read();
+                csv.ReadHeader();
+                Console.WriteLine(string.Join(",",csv.HeaderRecord!));
+                // 内容取得
+                foreach (var record in csv.GetRecords<Data>())
                 {
-                    var records = csv.GetRecords<Data>();
-                    
-                    foreach (var i in records)
-                    {
-                       Console.WriteLine($"{i.受注コード},{i.購入者名1},{i.購入者名2}");
-                       //Console.WriteLine($"{i.ColumnName}");
-                    }
+                    Console.WriteLine($"{record.受注コード},{record.購入者名1},{record.購入者名2}");
                 }
+
             }
+
         }
         public class Data
         {
-            public string 受注コード { get; set; }
+            public string 受注コード　{ get; set; }
             public string 購入者名1 { get; set; }
-
             public string 購入者名2 { get; set; }
-
         }
     }
 }
-
-
