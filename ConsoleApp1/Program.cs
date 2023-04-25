ffbb442b9bd275957021b4b8b8cba0a432c67883
samatamakiko <makiko.samata@keycrew.co.jp>
2023-04-25 10:55:09 +0900

    CSVファイルを配列に格納

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,10 @@ using System.Text;
 using CsvHelper;
 using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
 
 namespace test
 {
@@ -12,25 +16,21 @@ namespace test
     {
         static void Main(string[] args)
         {
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = true,
-            };
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-            using (var reader = new StreamReader(@"C:\Users\makik\Downloads\test.csv", Encoding.GetEncoding("Shift-JIS"))) 
-            using (var csv = new CsvReader(reader, config))
+            StreamReader sr = new StreamReader(@"C:\Users\makik\Downloads\test.csv", Encoding.GetEncoding("Shift-JIS"));
+            while(!sr.EndOfStream)
             {
-                //ヘッダー取得
-                csv.Read();
-                csv.ReadHeader();
-                Console.WriteLine(string.Join(",",csv.HeaderRecord!));
-                // 内容取得
-                foreach (var record in csv.GetRecords<Data>())
+                string line = sr.ReadLine();
+                string[] values = line.Split(',');
+                List<string> lists = new List<string>();
+                lists.AddRange(values);
+                foreach(string value in lists)
                 {
-                    Console.WriteLine($"{record.受注コード},{record.購入者名1},{record.購入者名2}");
+                    Console.Write("{0}",value);
                 }
-
+                Console.WriteLine();
             }
+            Console.ReadKey();
 
         }
         public class Data
@@ -41,3 +41,27 @@ namespace test
         }
     }
 }
+
+
+
+/* var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+            };
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var reader = new StreamReader(@"C:\Users\makik\Downloads\test.csv", Encoding.GetEncoding("Shift-JIS")))
+            using (var csv = new CsvReader(reader, config))
+            {
+                var data = new List<Data>();
+                var lines = File.ReadAllLines(reader);
+                Console.WriteLine(data);
+                //ヘッダー取得
+                csv.Read();
+                csv.ReadHeader();
+                Console.WriteLine(string.Join(",", csv.HeaderRecord!));
+                // 内容取得
+                foreach (var record in csv.GetRecords<Data>())
+                {
+                    Console.WriteLine($"{record.受注コード},{record.購入者名1},{record.購入者名2}");
+                }
+            }*/
